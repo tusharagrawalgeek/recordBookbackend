23d570cdef7efa7f5f9ab5c0ea5e12a9573f1213
tusharagrwall <tusharagrawalgeek@gmail.com>
2023-02-26 13:22:25 +0530

    Revert "channges"
    This reverts commit f465e7659ab894e67e7352dfce3d373fd21afdc5.

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors=require('cors')
 
 const app=express();
 
-const Customer=require('./schema');
+const Item=require('./schema');
+const User=require('./userSchema');
 const db='mongodb+srv://tushar:tushar432@cluster0.pvtih2d.mongodb.net/db2?retryWrites=true&w=majority';
 mongoose.connect(db,{
     // useNewUrlParser:true,
@@ -25,7 +26,7 @@ app.use(express.json());
 app.use(cors());
 
 app.post('/setitem',(req,res)=>{
-    const p=new Customer(req.body)
+    const p=new Item(req.body)
     p.save().then(()=>{
         res.send("Successfully added "+p.name);
     }).catch((err)=>{
@@ -34,7 +35,7 @@ app.post('/setitem',(req,res)=>{
 })
 app.get('/getitem', async (req,res,next)=>{
   try{
-    const data = await Customer.find();
+    const data = await Item.find();
     return res.status(200).json({
       success: true,
       count: data.length,
@@ -45,11 +46,31 @@ app.get('/getitem', async (req,res,next)=>{
     res.status(500).json({ error: 'server error' });
   }
 });
-// app.delete('/deleteitem/:id', async(req,res)=>{
-//     const result=await Item.findByIdAndDelete(req.params.id)
-//     res.json(result)
-// })
-
+app.delete('/deleteitem/:id', async(req,res)=>{
+    const result=await Item.findByIdAndDelete(req.params.id)
+    res.json(result)
+})
+app.get('/getuser', async (req,res,next)=>{
+    try{
+      const data = await User.find();
+      return res.status(200).json({
+        success: true,
+        count: data.length,
+        data: data,
+      });
+    } catch(err) {
+      console.log(err);
+      res.status(500).json({ error: 'server error' });
+    }
+  });
+  app.post('/setuser',(req,res)=>{
+    const p=new User( )
+    p.save().then(()=>{
+        res.send("Successfully added "+p.name);
+    }).catch((err)=>{
+        console.log(err);
+    })
+})
 app.listen(3001,()=>{
     console.log("Server is listening at port 3001");
 })
