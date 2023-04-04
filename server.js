f14d1bd351c8645c64b4f04caa80ec8cfc1381cb
tusharagrwall <tusharagrawalgeek@gmail.com>
2023-04-05 01:49:16 +0530

    fd

--- server.js
+++ server.js
@@ -1,142 +1,165 @@
-const express=require('express')
-const mongoose=require('mongoose')
-const cors=require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-const app=express();
+const app = express();
 
-const Item=require('./schema');
-const User=require('./userSchema');
-const ExportedItem=require('./exported');
-const ImportedItem=require('./importedItem');
+const Item = require("./schema");
+const User = require("./userSchema");
+const ExportedItem = require("./exported");
+const ImportedItem = require("./importedItem");
 // const db='mongodb+srv://tushar:tushar432@cluster0.pvtih2d.mongodb.net/db2?retryWrites=true&w=majority';
-const db='mongodb+srv://tushar:tushar123@cluster0.bnlzgl7.mongodb.net/rishi?retryWrites=true&w=majority';
+const db =
+  "mongodb+srv://tushar:tushar123@cluster0.bnlzgl7.mongodb.net/rishi?retryWrites=true&w=majority";
 // mongodb+srv://tushar:<password>@cluster0.pvtih2d.mongodb.net/?retryWrites=true&w=majority
-mongoose.connect(db,{
+mongoose
+  .connect(db, {
     // useNewUrlParser:true,
     // useCreateIndex: true,
     // useUnifiedTopology:true,
     // useFindAndModify:false
-}).then(()=>{
+  })
+  .then(() => {
     console.log("Connection established");
-}).catch(err=>{
-  console.log(err);
+  })
+  .catch((err) => {
+    console.log(err);
     console.log("Couldn't connect to database");
-})
+  });
 
-const bp=require('body-parser');
+const bp = require("body-parser");
 app.use(bp.json());
-app.use(bp.urlencoded({extended:true}))
+app.use(bp.urlencoded({ extended: true }));
 
 app.use(express.json());
 app.use(cors());
 
-app.post('/setitem',(req,res)=>{
-    const p=new Item(req.body)
-    const q=new ImportedItem(req.body)
-    var result=200;
-    p.save().then(()=>{
-        console.log("added to current inventory");
-    }).catch((err)=>{
+//adds and item to both curent inventory and imported item if id is null and if id is given then the item with the given id is updated and also added in the imported items
+app.post("/setitem", (req, res) => {
+    var result = 200;
+    // if(req.params.id===-1){
+      const p = new Item(req.body);
+  p.save()
+    .then(() => {
+      console.log("added to current inventory");
+    })
+    .catch((err) => {
       console.log("Could not add");
-      res.status(500).json({ error: 'server error' });
-    })
-    q.save().then(()=>{
-      console.log("added to importeditems");
-    }).catch((err)=>{
-      res.status(500).json({ error: 'server error' });
-    })
-    console.log(result);
-    res.send(result)
-})
+      res.status(500).json({ error: "server error" });
+    });
+    // }else{
+      // const result = Item.findByIdAndUpdate(req.params.id, req.body);
+      // console.log(result);
+    // }
+  console.log(result);
+  res.send(result);
+});
 
-app.get('/getimporteditems',async(req,res)=>{
-  try{
-    const data=await ImportedItem.find();
+app.post("/setimporteditem", (req, res) => {
+  const q = new ImportedItem(req.body);
+  var result = 200;
+  q.save()
+    .then(() => {
+      console.log("added to importeditems");
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "server error" });
+    });
+    res.send(result);
+}
+)
+app.get("/getimporteditems", async (req, res) => {
+  try {
+    const data = await ImportedItem.find();
     return res.status(200).json({
       success: true,
       count: data.length,
       data: data,
     });
-  }catch{
+  } catch {
     // console.log(err);
-    res.status(500).json({ error: 'server error' });
+    res.status(500).json({ error: "server error" });
   }
-})
-app.get('/getitem', async (req,res,next)=>{
-  try{
+});
+app.get("/getitem", async (req, res, next) => {
+  try {
     const data = await Item.find();
     return res.status(200).json({
       success: true,
       count: data.length,
       data: data,
     });
-  } catch(err) {
+  } catch (err) {
     console.log(err);
-    res.status(500).json({ error: 'server error' });
+    res.status(500).json({ error: "server error" });
   }
 });
-app.delete('/deleteitem/:id', async(req,res)=>{
-      const result=await Item.findByIdAndDelete(req.params.id)
-    res.send("Deleted")
-})
-app.delete('/deleteimporteditem/:id', async(req,res)=>{
-  const result=await ImportedItem.findByIdAndDelete(req.params.id)
-res.send("Deleted")
-})
-app.delete('/deleteexporteditem/:id', async(req,res)=>{
-  const result=await ExportedItem.findByIdAndDelete(req.params.id)
-res.send("Deleted")
-})
-app.get('/getuser', async (req,res,next)=>{
-    try{
-      const data = await User.find();
-      return res.status(200).json({
-        success: true,
-        count: data.length,
-        data: data,
-      });
-    } catch(err) {
-      console.log(err);
-      res.status(500).json({ error: 'server error' });
-    }
-  });
-  app.put('/update/:id',async (req,res)=>{
-    console.log(req.params.id,req.body);
-    const result=await Item.findByIdAndUpdate(req.params.id,req.body);
-    res.send("Updated data"+result)
-  })
-  app.post('/exportitem',(req,res)=>{
-    const q=new ExportedItem(req.body)
-    var result=200;
-    q.save().then(()=>{
+app.delete("/deleteitem/:id", async (req, res) => {
+  const result = await Item.findByIdAndDelete(req.params.id);
+  res.send("Deleted");
+});
+app.delete("/deleteimporteditem/:id", async (req, res) => {
+  const result = await ImportedItem.findByIdAndDelete(req.params.id);
+  res.send("Deleted");
+});
+app.delete("/deleteexporteditem/:id", async (req, res) => {
+  const result = await ExportedItem.findByIdAndDelete(req.params.id);
+  res.send("Deleted");
+});
+app.get("/getuser", async (req, res, next) => {
+  try {
+    const data = await User.find();
+    return res.status(200).json({
+      success: true,
+      count: data.length,
+      data: data,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "server error" });
+  }
+});
+app.put("/update/:id", async (req, res) => {
+  console.log(req.params.id, req.body);
+  const result = await Item.findByIdAndUpdate(req.params.id, req.body);
+  res.send("Updated data" + result);
+});
+app.post("/exportitem", (req, res) => {
+  const q = new ExportedItem(req.body);
+  var result = 200;
+  q.save()
+    .then(() => {
       console.log("added to exported items");
-    }).catch((err)=>{
-      res.status(500).json({ error: 'server error' });
     })
-    console.log(result);
-    res.send(result)
-  })
-  app.get('/getexported', async (req,res,next)=>{
-    try{
-      const data = await ExportedItem.find();
-      return res.status(200).json({
-        success: true,
-        count: data.length,
-        data: data,
-      });
-    } catch(err) {
+    .catch((err) => {
+      res.status(500).json({ error: "server error" });
+    });
+  console.log(result);
+  res.send(result);
+});
+app.get("/getexported", async (req, res, next) => {
+  try {
+    const data = await ExportedItem.find();
+    return res.status(200).json({
+      success: true,
+      count: data.length,
+      data: data,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "server error" });
+  }
+});
+app.post("/setuser", (req, res) => {
+  const p = new User();
+  p.save()
+    .then(() => {
+      res.send("Successfully added " + p.name);
+    })
+    .catch((err) => {
       console.log(err);
-      res.status(500).json({ error: 'server error' });
-    }
-  });
-  app.post('/setuser',(req,res)=>{
-    const p=new User( )
-    p.save().then(()=>{
-        res.send("Successfully added "+p.name);
-    }).catch((err)=>{
-        console.log(err);
-    })
-})
-app.listen(3001,()=>{
-    console.log("Server is listening at port 3001");
-})
+    });
+});
+app.listen(3001, () => {
+  console.log("Server is listening at port 3001");
+});
